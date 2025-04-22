f9af365a3da550ff854fd866817d4e2d9f490423
Amit Goren <animalfarm@mailfence.com>
2025-04-22 20:54:08 +0300

    refactor: add an underscore in front of an unused parameter

--- src/backtesting_market/fetcher/questdb_fetcher.rs
+++ src/backtesting_market/fetcher/questdb_fetcher.rs
@@ -108,7 +108,7 @@ pub enum Error {
 }
 
 impl std::fmt::Display for QuestDbFetcher {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    fn fmt(&self, _f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         todo!()
     }
 }
