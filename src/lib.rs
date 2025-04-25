26d550df1631ae697f385234253cf61a1d672d78
Amit Goren <animalfarm@mailfence.com>
2025-04-25 14:40:44 +0300

    chore: remove redundant dependencies and nightly features

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,3 @@
-// TODO some are not necessary anymore
-#![feature(async_iterator)]
-#![feature(sync_unsafe_cell)]
-#![feature(let_chains)]
-#![feature(btree_cursors)]
-
 mod algorithm;
 pub mod market;
 
